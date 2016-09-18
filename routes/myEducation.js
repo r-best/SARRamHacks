1b062facdc8e517c6aaaac0fa39a7861895fe127
thaden123 <thaden123@gmail.com>
2016-09-18 04:51:57 -0400

    added path resolve to all routes

--- routes/myEducation.js
+++ routes/myEducation.js
@@ -4,10 +4,13 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var path = require('path');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var json = JSON.parse(fs.readFileSync('brock.json', 'utf8'));
+    var pathToJson = path.resolve('brock.json');
+    var json = JSON.parse(fs.readFileSync(pathToJson , 'utf8'));
     res.render('myEducation', { data: json, title: 'my Education'});
 });
 
