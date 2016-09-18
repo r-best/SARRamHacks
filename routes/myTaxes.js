22a6f1554f13f386a359ed9b28414e2a83501d79
Justin Gardner <jgardner@me-xl.us>
2016-09-18 02:43:53 -0400

    Fixing brock.json miss

--- routes/myTaxes.js
+++ routes/myTaxes.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var json = JSON.parse(fs.readFileSync('..\\brock.json', 'utf8'));
+	var json = JSON.parse(fs.readFileSync('brock.json', 'utf8'));
     res.render('myTaxes', { data: json, title: 'My Taxes'});
 });
 
