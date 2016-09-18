ec5dc92dd192c6ccf8b03e4a3145e91862480aff
thaden123 <thaden123@gmail.com>
2016-09-18 03:58:53 -0400

    3:59 from Tyler

--- routes/myPersonalInfo.js
+++ routes/myPersonalInfo.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {;
-    var json = JSON.parse(fs.readFileSync('brock.json', 'utf8'));
+    var json = JSON.parse(fs.readFileSync('/SARRamhacks/brock.json', 'utf8'));
     res.render('myPersonalInfo', { data: json, title: 'My Personal Infos'});
 });
 
